975899a1588a73653eff4afce872e9af47baaed8
7118-eth <7118.eth@proton.me>
2025-08-25 05:19:32 +0400

    fix(docs): update hash for docs index.mdx import to reflect recent changes

--- apps/docs/.source/index.ts
+++ apps/docs/.source/index.ts
@@ -1,5 +1,5 @@
 // @ts-nocheck -- skip type checking
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1756083812372"
+import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1756083877489"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
 export const docs = _runtime.doc<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"/home/user/git/2025_2/wavehack/merke-am/merke-am-mono/apps/docs/content/docs/index.mdx"}, data: docs_0 }]);
