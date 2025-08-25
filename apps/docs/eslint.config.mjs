1ae335867c6b1cb6f1c1f2f908bdfb6bc22ebbbe
7118-eth <7118.eth@proton.me>
2025-08-25 05:14:53 +0400

    chore(eslint): add docs flat config; ignore build outputs; disable prop-types in landing

---
+++ apps/docs/eslint.config.mjs
@@ -0,0 +1,15 @@
+import { nextJsConfig } from "@repo/eslint-config/next-js";
+
+/** @type {import("eslint").Linter.Config} */
+export default [
+  ...nextJsConfig,
+  {
+    ignores: [
+      ".next/**",
+      ".source/**",
+      "next-env.d.ts",
+    ],
+  },
+];
+
+
