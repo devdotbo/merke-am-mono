c2abc09ce2a67d284ea22d7c0cf78b26bc3c20a3
devbotbo <7118.eth@proton.me>
2025-08-26 14:09:51 +0200

    chore(docs): remove obsolete source files and update .gitignore to include .source directory

--- apps/docs/.source/index.ts
+++
@@ -1,6 +0,0 @@
-// @ts-nocheck -- skip type checking
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1756131536652"
-import { _runtime } from "fumadocs-mdx"
-import * as _source from "../source.config"
-export const docs = _runtime.doc<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"/home/user/git/2025_2/wavehack/merke-am/merke-am-mono/apps/docs/content/docs/index.mdx"}, data: docs_0 }]);
-export const meta = _runtime.meta<typeof _source.meta>([]);
