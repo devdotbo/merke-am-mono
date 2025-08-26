a9c23c43aabf7317977e6a54f4cc653a3ee13722
devbotbo <7118.eth@proton.me>
2025-08-26 14:32:40 +0200

    feat(app): integrate cookie storage into Wagmi adapter configuration for improved session management

--- apps/app/src/config/index.ts
+++ apps/app/src/config/index.ts
@@ -1,6 +1,7 @@
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { mainnet, arbitrum } from '@reown/appkit/networks'
 import type { AppKitNetwork } from '@reown/appkit/networks'
+import { cookieStorage, createStorage } from 'wagmi'
 
 // Get projectId from https://cloud.reown.com
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "";
@@ -13,6 +14,7 @@ export const networks = [mainnet, arbitrum] as [AppKitNetwork, ...AppKitNetwork[
 
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
+  storage: createStorage({ storage: cookieStorage }),
   ssr: true,
   projectId,
   networks
